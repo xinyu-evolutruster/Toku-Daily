3a5f1db55394ad8f33fe53c29763e053be779fb4
unknown <xinyuliu0517@gmail.com>
2022-08-30 15:42:30 -0400

    added a js module

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("Hello from our first js file!");
+
+var rand = Math.random();
+
+if (rand < 0.5) {
+    console.log("Random number is less than 0.5");
+}
+
+prom = prompt("well")
+if (!prom) {
+    console.log("TRUTHY");
+}
+
+let lottoNums = [19, 22, 45, 12, 3];
+// console.log(lottoNums.sum());
