958c9ec0adf781b19ad8341aa95d29916747a49c
unknown <xinyuliu0517@gmail.com>
2022-08-30 23:30:06 -0400

    a little more complicated js program

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-console.log("Hello from our first js file!");
+var input = prompt("What would you like to do?");
 
-var rand = Math.random();
-
-if (rand < 0.5) {
-    console.log("Random number is less than 0.5");
+const todos = ["collect chicken eggs", "clean litter box"];
+while (input !== "quit" && input !== "Q") {
+    console.log(`input is ${input}`);
+    if (input === "list") {
+        console.log("***************");
+        for (var i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log("***************");
+    }
+    else if (input === "new") {
+        const newTodo = prompt("OK, what is the new todo?");
+        todos.push(newTodo);
+        console.log(`${newTodo} is added to the list`);
+    }
+    else if (input === "delete") {
+        const index = parseInt(prompt("Ok, enter an index to delete"));
+        if (!Number.isNaN(index)) {
+            todos.splice(index, 1);
+            console.log(`todo ${index} is deleted`);
+        }
+        else {
+            console.log("unknown index");
+        }
+    }
+    input = prompt("what would you like to do?")
 }
 
-prom = prompt("well")
-if (!prom) {
-    console.log("TRUTHY");
-}
-
-let lottoNums = [19, 22, 45, 12, 3];
-// console.log(lottoNums.sum());
+console.log("OK QUIT THE APP");
